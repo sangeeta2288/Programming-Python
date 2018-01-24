1d4e66479f6fd5e018c35150535891476056b31f
sangeeta2288 <ashrit.sangeeta@gmail.com>
2018-01-24 14:50:34 -0500

    List Vs Tuple creation time

--- compute_execution_time.py
+++ compute_execution_time.py
@@ -10,3 +10,5 @@ print(list_test)
 # Compute the time required to create tuple(1,2,3,4,5) same over a million number of times
 tuples_test = timeit.timeit(stmt="(1,2,3,4,5)", number = 1000000)
 print(tuples_test)
+
+# Hence we can conclude that, even though tuples are immutable they are fast in their operations. And also they consume less memory when comapred with Lists.

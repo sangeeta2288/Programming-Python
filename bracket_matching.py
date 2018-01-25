dfe02a5ec5a273744e469fe406246a69186ff3d1
sangeeta2288 <ashrit.sangeeta@gmail.com>
2018-01-24 20:44:31 -0500

    Valid_Parantheses

---
+++ bracket_matching.py
@@ -0,0 +1,18 @@
+class Solution:
+	def isValid(self,s):
+		mystack = []
+		dict = {"}":"{","]":"[",")":"("}
+		for item in s:
+			if item in dict.values():
+				mystack.append(item)
+			elif item in dict.keys():
+				if(mystack == [] or dict[item] != mystack.pop()):
+					return False
+				else:
+					return False
+		return mystack==[]		
+
+solution = Solution()
+input = input("Enter pattern==> ")
+res = solution.isValid(input)
+print(res)            

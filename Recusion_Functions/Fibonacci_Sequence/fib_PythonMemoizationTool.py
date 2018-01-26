967216c22ecb3dae60a2736ea0187109a2bd77e1
sangeeta2288 <ashrit.sangeeta@gmail.com>
2018-01-25 21:39:01 -0500

    validating input

--- Recusion_Functions/Fibonacci_Sequence/fib_PythonMemoizationTool.py
+++ Recusion_Functions/Fibonacci_Sequence/fib_PythonMemoizationTool.py
@@ -5,6 +5,7 @@ from functools import lru_cache
 
 @lru_cache(maxsize=200)
 def fibonacci(n):
+	validate_input(n)
 	if(n==1):
 		return 1
 	elif(n==2):
@@ -12,6 +13,12 @@ def fibonacci(n):
 	elif(n>2):
 		return(fibonacci(n-1)+fibonacci(n-2))
 
+def validate_input(n):
+	if(type(n) != int):
+		raise TypeError("n should be positive a integer")
+	elif(n<1):
+		raise ValueError("n should be a positive integer")
+
 for n in range(1,101):
-	print(n,":",fibonacci(n))
+        print(n,":",fibonacci(n))
 
